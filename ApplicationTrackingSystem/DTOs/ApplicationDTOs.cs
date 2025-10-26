f8ca0f737ba18d80b947f47da9d7fd99f81c26d4
Krishnakanth KK <bhoopalkarthi@gmail.com>
2025-10-26 17:14:46 +0530

    DTOs code

---
+++ ApplicationTrackingSystem/DTOs/ApplicationDTOs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApplicationTrackingSystem.Core.DTOs
+{
+    public class CreateApplicationRequest
+    {
+        public int JobRoleId { get; set; }
+        public string Notes { get; set; }
+    }
+
+    public class UpdateApplicationStatusRequest
+    {
+        public int ApplicationId { get; set; }
+        public int NewStatus { get; set; }
+        public string Comment { get; set; }
+    }
+
+    public class ApplicationResponse
+    {
+        public int Id { get; set; }
+        public string ApplicantName { get; set; }
+        public string JobRoleTitle { get; set; }
+        public string Status { get; set; }
+        public DateTime AppliedAt { get; set; }
+        public DateTime? LastUpdatedAt { get; set; }
+        public string Notes { get; set; }
+        public List<ActivityLogResponse> ActivityLogs { get; set; }
+    }
+
+    public class ActivityLogResponse
+    {
+        public int Id { get; set; }
+        public string PreviousStatus { get; set; }
+        public string NewStatus { get; set; }
+        public string Comment { get; set; }
+        public string UpdatedByRole { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ ApplicationTrackingSystem/DTOs/AuthDTOs.cs
@@ -0,0 +1,24 @@
+﻿namespace ApplicationTrackingSystem.Core.DTOs
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class RegisterRequest
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; } // 1=Applicant, 2=BotMimic, 3=Admin
+    }
+}
